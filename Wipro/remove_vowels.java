f935ba7b5357a17baa068d6d50294931b7485967
KhushviB <khushvi.bamrolia28@gmail.comn>
2024-09-01 23:19:04 +0530

    This removes vowels from a string

--- Wipro/remove_vowels.java
+++ Wipro/remove_vowels.java
@@ -2,7 +2,7 @@ public class remove_vowels {
     public static void main(String args[]){
         char str[] = "Hello Worlda!!".toCharArray();
         for(int i =0; i < str.length; i++){
-            if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
+            if(str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
                 continue;
             }else {
                 System.out.print(str[i]);
